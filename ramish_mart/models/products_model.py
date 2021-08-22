66f335d14a836ff0cb760c646d5af02fbded714b
RamishR <ramish@shipr.express>
2021-08-22 07:01:18 -0700

    Created model/schema for products table using orm

---
+++ ramish_mart/models/products_model.py
@@ -0,0 +1,15 @@
+import uuid, datetime
+from ramish_mart import db
+
+class Products(db.Model):
+
+    id = db.Column(db.String(100), default=lambda: str(uuid.uuid4()), primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False) 
+    created = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+db.create_all()
