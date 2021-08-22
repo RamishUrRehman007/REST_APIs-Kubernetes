4c64f2148cd8ff6294fd20aacba7f9064313d727
RamishR <ramish@shipr.express>
2021-08-22 13:16:05 -0700

    Used cross_origin decorator and handled common exceptions

--- ramish_mart/services/products_service.py
+++ ramish_mart/services/products_service.py
@@ -3,16 +3,21 @@ from ramish_mart import db
 from ramish_mart.models.products_model import Products, get_product_object
 
 def addProduct(name, price):
-    _product = Products(
-            name=name,
-            price=price
-        )
 
-    db.session.add(_product)
-    db.session.commit()
+    try:
+        _product = Products(
+                name=name,
+                price=price
+            )
+
+        db.session.add(_product)
+        db.session.commit()
+        
+        product = utils.single_object_to_dict(
+                        Products.query.filter_by(name=name).first()
+                    )
+        
+        return {'Message': "Product Added Successfully" , 'Product' : product}
     
-    product = utils.single_object_to_dict(
-                    Products.query.filter_by(name=name).first()
-                )
-    
-    return {'Message': "Product Added Successfully" , 'Product' : product}
+    except Exception as e:
+        return {'Message': str(e)}
